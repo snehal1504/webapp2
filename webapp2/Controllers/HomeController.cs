740ebfbefe0232da41b66143727a5ef071df3e41
Snehal <snehal_ucer123@outlook.com>
2021-05-26 17:21:57 +0530

    Add project files.

---
+++ webapp2/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using webapp2.Models;
+
+namespace webapp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            ViewBag.MyProp = "Snehal";
+            ViewBag.MyList = new List<string>() { "View", "Bag", "Dynamic", "Property" };
+            List<Category> clist = new List<Category>()
+            {
+                new Category(){ Name = "Packet" , Id=11, DisplayOrder = 41},
+                new Category(){ Name = "Packet1" , Id=12, DisplayOrder = 42},
+                new Category(){ Name = "Packet2" , Id=13, DisplayOrder = 43},
+                new Category(){ Name = "Packet3" , Id=14, DisplayOrder = 44}
+            };
+            ViewBag.MyClist = clist;
+
+
+            ViewData["MyKey"] = "This is View Data!!";
+
+            ViewData["MyList"] = new List<string>() { "View", "Data", "Works", "Dictionary" };
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ webapp2/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using webapp2.Models;
+
+namespace webapp2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+        // public DbSet<ApplicationType> ApplicationType { get; set; }
+    }
+}
